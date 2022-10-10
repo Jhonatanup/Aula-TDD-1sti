4585722b3fcd2e29e3b726c4b4cac15f37917f0b
jhonatanup <jleandroweb@gmail.com>
2022-10-10 14:07:54 -0400

    Escrevendo os Testes do Component App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Renderiza no maximo 3 items por pagina", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const posts = await screen.findAllByTestId("post-item");
+  expect(posts.length).toBeLessThan(4);
+});
+
+test("Renderiza um botao de prev", async () => {
+  render(<App />);
+  const prevButton = await screen.findByTestId("prev-button");
+  expect(prevButton).toBeInTheDocument();
+});
+
+test("Renderiza um botao de next", async () => {
+  render(<App />);
+  const nextButton = await screen.findByTestId("next-button");
+  expect(nextButton).toBeInTheDocument();
+});
+
+test("Quando e somente quando a lista estiver no inicial o botao de prev fica desativado", async () => {
+  render(<App initialPage={true} />);
+  const prevButton = await screen.findByTestId("prev-button");
+  expect(prevButton).toBeDisabled();
+});
+
+test("Quando e somente quando a lista estiver no inicial o botao de next fica desativado", async () => {
+  render(<App finalPage={true} />);
+  const nextButton = await screen.findByTestId("next-button");
+  expect(nextButton).toBeDisabled();
 });
