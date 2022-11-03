1f7f49ffdcfa5b505414a13c87d795ee267e8d06
jhonatanup <jleandroweb@gmail.com>
2022-11-03 14:32:37 -0400

    Implementamos a paginaçao atraves do state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-function App() {
+import { useState } from "react";
+import { preparaListaASerExibida } from "./utils";
+const NUMERO_ITEMS_PAGINA = 3;
+
+function App({ list, initialPage, finalPage }) {
+  const [pagina, setPagina] = useState(1);
+  const listaASerExibida = preparaListaASerExibida(
+    list,
+    pagina,
+    NUMERO_ITEMS_PAGINA
+  );
+
+  const lidaComODecrescimoNoNumeroDePagina = () => {
+    //TODO: Jogar a logica para o utils setPagina(funcaoquecrieietestei())
+    if (pagina === 1) {
+      return;
+    }
+    setPagina(pagina - 1);
+  };
+
+  const lidaComOAcrescimoNoNumeroDePagina = () => {
+    setPagina(pagina + 1);
+  };
+
   return (
     <div>
       <ul>
-        <li data-testid="post-item">1</li>
-        <li data-testid="post-item">1</li>
-        <li data-testid="post-item">1</li>
+        {listaASerExibida?.map((post) => (
+          <li data-testid="post-item" key={post}>
+            {post}
+          </li>
+        ))}
       </ul>
-      <button data-testid="prev-button">prev</button>
-      <button data-testid="next-button">next</button>
+      <button
+        data-testid="prev-button"
+        disabled={pagina === 1}
+        onClick={lidaComODecrescimoNoNumeroDePagina}
+      >
+        prev
+      </button>
+      <button
+        data-testid="next-button"
+        // disabled={() => console.log("TODO: implementar checaSeTaNoFinal")}
+        onClick={lidaComOAcrescimoNoNumeroDePagina}
+      >
+        next
+      </button>
     </div>
   );
 }
