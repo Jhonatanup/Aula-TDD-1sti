999e85e3d1dade515c6708485a32c311a6391fba
jhonatanup <jleandroweb@gmail.com>
2022-11-03 14:31:51 -0400

    Passamos a lista como props pro component App

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const lista = [0, 1, 2, 3, 4, 5, 6];
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App list={lista} />
   </React.StrictMode>
 );
 
